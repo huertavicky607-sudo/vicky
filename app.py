6ce1f4bfc6b417f912d7e4249d0378ee84b4e815
huertavicky607-sudo <huertavicky607@gmail.com>
2026-09-21 17:45:15 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template_string, jsonify
 import os
 
-app = Flask(__app__)
+app = Flask(__name__)
 
 # HTML Template using Tailwind CSS for styling
 HTML_TEMPLATE = """
